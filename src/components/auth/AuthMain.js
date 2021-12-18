14ab1023f32e7007e4347d5103e49ff3e32c3508
JohnDan72 <mr._dany@hotmail.com>
2021-12-17 19:16:59 -0600

    Add linear-gradient para fondo de login y register, (se modifican algunos parámetros del container)

--- src/components/auth/AuthMain.js
+++ src/components/auth/AuthMain.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+
 const AuthMain = () => {
     return (
-        <div className='auth__container '>
+        <div className='auth__container my-4'>
             <div className='my-2 mx-3'>
                 <Outlet />
 
