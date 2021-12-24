5e3eb9b40e02bf0aa2f64f86d5bf5fd55b2b45fe
JohnDan72 <mr._dany@hotmail.com>
2021-12-24 10:26:07 -0600

    Se añade lista de notas y form para agregar una

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -7,15 +7,28 @@ const defaultProps = {};
 
 
 const JournalScreen = () => {
+
+    const handleNota = () => {
+        console.log("handle nota")
+    }
     return (
         <div className='journal__main'>
             <div className='row allVH'>
                 <div className='col-12 col-md-4 journal__sidebar'>
-                    <Sidebar className='col-12 col-md-4'/>
+                    <Sidebar className='col-12 col-md-4' />
                 </div>
                 <div className='journal__container col-12 col-md-8'>
                     <div className='container my-4'>
                         <h2> Journal Screen </h2>
+
+                        <form className='row mt-4' onSubmit={handleNota}>
+                            <section className='col-12 col-md-8 my-auto'>
+                                <input type="nota" className="form-control" id="id_nota" placeholder="tu nota" />
+                            </section>
+                            <section className='col-12 col-md-4 my-auto text-center'>
+                                <button type="submit" className='btn btn-primary'> Agregar nota </button>
+                            </section>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -5,12 +5,26 @@ const Sidebar = () => {
 
     return (
         <div className="container my-4">
-            <div className='row'>
+            <div className='row mt-4'>
                 <div className='col align-self-start my-auto'>
                     <span> Juan Daniel Garcia </span>
                 </div>
                 <div className='col align-self-end text-end'>
                     <Link className='btn btn-primary' to={`/auth`}>Log-out</Link>
+                </div>
+            </div>
+            <div className='row mt-4'>
+                <div className="col-12 alert alert-dark" role="alert">
+                    Lista de notas 1
+                </div>
+                <div className="col-12 alert alert-dark" role="alert">
+                    Lista de notas 2
+                </div>
+                <div className="col-12 alert alert-dark" role="alert">
+                    Lista de notas 3
+                </div>
+                <div className="col-12 alert alert-dark" role="alert">
+                    Lista de notas 4
                 </div>
             </div>
         </div>
